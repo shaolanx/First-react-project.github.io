a84f477bde28bd46baf898642cc9617ce401d80f
shaolanx <daggerx@mail.ru>
2023-09-08 17:04:27 +0300

    08.09

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Header from './Header';
-import Technologies from './Technologies';
+// import Header from './Header';
+// import Technologies from './Technologies';
 
 
 const App = () => {
   return (
-    <div className="App App-header">
-      <img className='App-logo' src={logo} alt='123'></img>
-      <Technologies />
-      <Header />
+    <div className="app-wrapper">
+      <header className="header">
+        <img className='App-logo' src={logo} alt='123'></img>
+      </header>
+      <nav className="nav">
+        <a href='#s'>Profile</a>
+        <a href='#s'>Masseges</a>
+        <a href='#s'>Masseges</a>
+        <a href='#s'>Masseges</a>
+        <a href='#s'>Masseges</a>
+      </nav>
+      <div className="content">
+        Main Content
+      </div>  
     </div>
   );
 }
